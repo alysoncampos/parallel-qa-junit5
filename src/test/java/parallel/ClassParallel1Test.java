9875d6f231db90bf38b9011304be433fadfd0069
Alyson Campos <alyson.siqueiracampos@gmail.com>
2024-03-08 08:55:22 -0300

    add exemplo config testes paralelos com junit5

---
+++ src/test/java/parallel/ClassParallel1Test.java
@@ -0,0 +1,36 @@
+package parallel;
+
+import org.junit.jupiter.api.Test;
+
+public class ClassParallel1Test {
+
+    @Test
+    public void testMethodOne() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("methodOne");
+    }
+
+    @Test
+    public void testMethodTwo() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("methodTwo");
+    }
+
+    @Test
+    public void testMethodThree() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("methodThree");
+    }
+
+    @Test
+    public void testMethodFour() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("methodFour");
+    }
+
+    @Test
+    public void testMethodFive() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("methodFive");
+    }
+}
